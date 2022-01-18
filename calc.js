b96f553dbe83f26a947de677c38392382d6591a1
Cameron Zakkour <camzakkour@gmail.com>
2022-01-18 13:36:37 -0800

    Addition implemtation working

--- calc.js
+++ calc.js
@@ -14,7 +14,12 @@ reader.question("What would you like to calculate?", function(input){
 	num1 = Number(tokens[1]);
 	num2 = Number(tokens[2]);
 
-
+	if (mathSymbol === "+") {
+		console.log(num1 + num2)
+	}
+	// console.log(mathSymbol)
+	// console.log(num1)
+	// console.log(num2)
 	// This line closes the connection to the command line interface.
 	reader.close()
 
